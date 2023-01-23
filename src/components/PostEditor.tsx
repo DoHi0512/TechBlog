8479da43c95cea4c4dc4bb08daea84a33ff7840a
DoHi0512 <donghuni512@gmail.com>
2023-01-23 22:05:29 +0900

    Create:: 글 작성 컴포넌트

---
+++ src/components/PostEditor.tsx
@@ -0,0 +1,13 @@
+import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
+export default function PostEditor() {
+  return (
+    <Editor
+      initialValue="# 여기에 글 작성"
+      previewStyle="vertical"
+      height="800px"
+      initialEditType="markdown"
+      useCommandShortcut={true}
+    />
+  );
+}
