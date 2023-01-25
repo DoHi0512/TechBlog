5cfe0b8a93b849decb71bfd4ab8931a9e76aa833
DoHi0512 <donghuni512@gmail.com>
2023-01-25 20:31:09 +0900

    Update::글 작성 페이지 완성
    제목, markdown 에디터 생성

--- src/pageContainer/create/index.tsx
+++ src/pageContainer/create/index.tsx
@@ -1,16 +1,30 @@
 import * as S from "./style";
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 const PostEditor = dynamic(() => import("../../components/PostEditor"), {
   ssr: false,
 });
 export default function CreatePage() {
+  const [content, setContent] = useState<string>("");
+  console.log(content);
   return (
     <S.Layout>
-      <S.TitleBox>
-        <S.InputTitle />
-      </S.TitleBox>
-      <PostEditor />
+      <S.InputBox>
+        <S.Info>제목</S.Info>
+        <S.Title></S.Title>
+        <S.Info>내용</S.Info>
+        <PostEditor />
+        <S.Submit>
+          <S.Exit>
+            <AiOutlineArrowLeft />
+            <span>나가기</span>
+          </S.Exit>
+          <S.Finish>
+            <span>출간하기</span>
+          </S.Finish>
+        </S.Submit>
+      </S.InputBox>
     </S.Layout>
   );
 }
