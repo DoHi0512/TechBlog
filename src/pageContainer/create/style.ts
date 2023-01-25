e220492d3f530ad0140fb56fbc42ffc72f2bf6b8
DoHi0512 <donghuni512@gmail.com>
2023-01-25 20:31:32 +0900

    Update:: 글 작성 페이지 스타일 수정

--- src/pageContainer/create/style.ts
+++ src/pageContainer/create/style.ts
@@ -3,17 +3,58 @@ import styled from "styled-components";
 export const Layout = styled.div`
   width: 100%;
   height: 80rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
-export const InputTitle = styled.div`
-  border-right: 1px solid;
-  width: 50%;
+
+export const InputBox = styled.div`
+  width: 90%;
   height: 100%;
 `;
-export const TitleBox = styled.div`
-  border-top: 1px solid;
+export const Title = styled.input`
+  &:focus {
+    outline: none;
+  }
   width: 100%;
-  height: 12rem;
+  height: 4rem;
+  font-size: 2rem;
+  font-weight: bold;
+  border-radius: 0.5rem;
+  border: 1px solid gray;
 `;
-export const ShowTitle = styled.div`
-  width: 50%;
+export const Info = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-top: 3rem;
+  margin-bottom: 1rem;
+`;
+
+export const Submit = styled.div`
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items : center;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-top: 4rem;
+`;
+
+export const Exit = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const Finish = styled.div`
+  height: 80%;
+  width: 12rem;
+  border-radius: 0.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #12b886;
+  color: white;
 `;
