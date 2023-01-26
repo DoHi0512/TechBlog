b5d200566e41b9d17c06811cc217a639307e5fd2
DoHi0512 <donghuni512@gmail.com>
2023-01-26 21:20:41 +0900

    Create:: hover 미리보기 css

--- src/components/post/style.tsx
+++ src/components/post/style.tsx
@@ -1,8 +1,20 @@
 import styled, { keyframes } from "styled-components";
-
+export const Preview = styled.div`
+  width: 100%;
+  height: 80%;
+  display: none;
+  position: absolute;
+`;
+export const Img = styled.img`
+  width: 100%;
+  height: 80%;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+`;
 export const Post = styled.div`
+  position: relative;
   border: 1px solid;
-  border-radius: 1.5rem;
+  border-radius: 1rem;
   width: 100%;
   height: 23rem;
   grid-column: auto;
@@ -13,17 +25,19 @@ export const Post = styled.div`
     transform: translateY(-1rem);
     transition: 0.3s;
     cursor: pointer;
+    ${Preview} {
+      border-top-left-radius: 1rem;
+      border-top-right-radius: 1rem;
+      display: flex;
+      background-color: rgba(255, 255, 255, 0.9);
+      justify-content: center;
+    }
   }
   &:not(:hover) {
     transition: 0.3s;
   }
 `;
-export const Img = styled.img`
-  width: 100%;
-  height: 80%;
-  border-top-left-radius: 1.5rem;
-  border-top-right-radius: 1.5rem;
-`;
+
 export const Info = styled.div`
   width: 95%;
   height: 20%;
@@ -33,4 +47,17 @@ export const Info = styled.div`
   span {
     font-size: 1.3rem;
   }
+`;
+
+export const Desc = styled.div`
+  line-height: 2rem;
+  margin-top: 2rem;
+  font-size: 1.5rem;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 90%;
 `;
