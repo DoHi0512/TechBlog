f045af77d14908c5979b3681ce8ff9bc9c2bd0c3
DoHi0512 <donghuni512@gmail.com>
2023-02-02 18:29:07 +0900

    Create:: 포스트 뷰어 생성

---
+++ src/components/PostViewer.tsx
@@ -0,0 +1,8 @@
+import { Viewer } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+interface PostingProps {
+  content: string;
+}
+export default function PostViewer({ content }: PostingProps) {
+  return <Viewer initialValue={content} ></Viewer>;
+}
