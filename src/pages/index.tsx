2fa046e54acc6032f0bd7b46ab627fde8bd26d91
DoHi0512 <donghuni512@gmail.com>
2023-02-15 20:48:48 +0900

    Create:: getServerSideProps 이용해서 API 호출

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,18 @@
-import MainPage from "../pageContainer/main";
-export default function Home() {
-  return <MainPage />;
+import { GetServerSideProps } from "next";
+import { MainPage } from "../pageContainer/main";
+import { PostType } from "../type/post";
+import PostApi from "../api/post";
+import { AxiosType } from "../type/axios";
+
+export default function Home(data: AxiosType) {
+  return <MainPage {...data}/>;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data : AxiosType = await PostApi.getAll();
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data.data)),
+    },
+  };
+};
