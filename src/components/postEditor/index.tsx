8f8359633b206e2529fc6413ab871528ee48d290
DoHi0512 <donghuni512@gmail.com>
2023-02-18 21:05:24 +0900

    Fix:: never 타입 버그 수정
    useCallBack() 사용해서 getInstace() 버그 수정

--- src/components/postEditor/index.tsx
+++ src/components/postEditor/index.tsx
@@ -3,17 +3,18 @@ import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useRouter } from "next/dist/client/router";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import PostApi from "../../api/post";
 import { PostType } from "../../type/post";
 import Link from "next/dist/client/link";
+import { Editor as EditorType, EditorProps } from "@toast-ui/react-editor";
 interface UploadImage {
   file: File;
   thumbnail: string;
   type: string;
 }
 export default function PostEditor() {
-  const editorRef = useRef(null);
+  const editorRef = useRef<EditorType>(null);
   const router = useRouter();
   const [post, setPost] = useState<PostType>({
     content: "",
@@ -22,15 +23,25 @@ export default function PostEditor() {
     image: "",
   });
   // const [imageFile, setImageFile] = useState<UploadImage | null>(null);
-  const GetContent = () => {
+  // const GetContent = useCallback() => {
+  //   if (!editorRef.current) return;
+  //   console.log(editorRef.current);
+  //   const editorIns = editorRef.current.getInstance();
+  //   const editorMark = editorIns.getMarkdown();
+  //   const editorHtml = editorIns.getHTML();
+  //   console.log(editorHtml);
+  //   setPost({ ...post, content: editorMark });
+  // };
+  const GetContent = useCallback(() => {
     if (editorRef.current) {
+      const instance = editorRef.current.getInstance();
       const editorIns = editorRef.current.getInstance();
       const editorMark = editorIns.getMarkdown();
       const editorHtml = editorIns.getHTML();
       console.log(editorHtml);
       setPost({ ...post, content: editorMark });
     }
-  };
+  }, [, editorRef]);
   return (
     <S.Layout>
       <S.InputBox>
@@ -55,30 +66,6 @@ export default function PostEditor() {
           onChange={() => GetContent()}
         />
         <S.Submit>
-          {/* <input
-            type="file"
-            accept="image/png, image/jpeg, image/jpg"
-            onChange={(e) => {
-              const fileList = e.target.files;
-
-              if (fileList && fileList[0]) {
-               
-                const url = URL.createObjectURL(fileList[0]);
-                console.log(url)
-                setImageFile({
-                  file: fileList[0],
-                  thumbnail: url,
-                  type: fileList[0].type.slice(0, 5),
-                });
-                setPost({
-                  ...post,
-                  image: imageFile?.file,
-                });
-                console.log(post);
-              }
-            }}
-          /> */}
-
           <S.Btn
             bgcolor="white"
             txtcolor="black"
