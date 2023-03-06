32952925a858409ccd03c202640e0169f1199051
DoHi0512 <donghuni512@gmail.com>
2023-03-06 20:25:59 +0900

    Update:: 데이터 받아오는 방식 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,27 @@ import { MainPage } from "../pageContainer/main";
 import { PostType } from "../type/post";
 import PostApi from "../api/post";
 import { AxiosType } from "../type/axios";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { FirebaseType } from "../type/firebase";
 
-export default function Home(data: AxiosType) {
-  return <MainPage {...data}/>;
+export default function Home(data: FirebaseType[]) {
+  console.log(data.data);
+  return <MainPage {...data} />;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const data : AxiosType = await PostApi.getAll();
+  const data: FirebaseType | null = await PostApi.getAll();
+  const posts: FirebaseType[] = [];
+  data?.forEach((element: DocumentData) => {
+    posts.push(element.data());
+  });
   return {
     props: {
-      data: JSON.parse(JSON.stringify(data.data)),
+      data: posts,
     },
   };
 };
